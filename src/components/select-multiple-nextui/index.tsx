b5f3980e50362cf3e5ef855d6db3d205a25b46cc
Andre Pires <andre.figueiredo@aluno.faculdadeimpacta.com.br>
2023-11-09 12:15:44 -0300

    Novo select

---
+++ src/components/select-multiple-nextui/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Select, SelectItem , SelectSection} from "@nextui-org/react";
+import styles from './styles.module.css'
+import { Roles } from '@/model/roles';
+
+interface Props{
+    label: string ,
+    rolesLista: Roles[] , 
+    roles: Iterable<string> ,
+    handleResult : any
+}
+
+export default function App({ label, rolesLista , roles , handleResult   }: Props) {
+    console.log(roles);
+
+    const handleSelectionChange = (e : any) => {
+        handleResult(e.target.value);
+    };
+
+  return (
+
+    <div className={styles.divInput}>
+        <label className={styles.label}>{label}:</label>
+
+        <Select
+            className={styles.divInput}
+            placeholder="Selecione uma role"
+            selectionMode="multiple"
+            selectedKeys={roles}
+            onChange={handleSelectionChange}
+        >
+        {rolesLista.map((o) => (
+            <SelectItem key={o.name} value={o.name}>
+            {o.description}
+            </SelectItem>
+        ))}
+        </Select>
+    </div>
+  );
+}
--- src/components/select-multiple/index.tsx
+++ src/components/select-multiple/index.tsx
@@ -1,51 +1,24 @@
 import { DetailedHTMLProps, SelectHTMLAttributes } from 'react'
 import React from 'react'
-import Select from 'react-select'
 
 import styles from './styles.module.css'
 import { Roles } from '@/model/roles';
 
-// interface Props extends DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>{
-//     label: string ,
-//     rolesLista: Roles[] , 
-// }
-
-// export default function SelectMultiple({ label, rolesLista , ...rest }: Props){
-//     return(
-//         <div className={styles.divInput}>
-//             <label className={styles.label}>{label}:</label>
-//             <select className={styles.input} {...rest}>
-//             {rolesLista.map(o => (
-//                 <option key={o.name} value={o.name} >{o.description}</option>
-//             ))}
-//             </select>
-//         </div>
-//     )
-// }
-
-
-interface Props {
-    label: string;
-    rolesLista: Roles[];
-    value: string[];
-    onChange: (selectedOptions: { value: string; label: string }[]) => void;
+interface Props extends DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>{
+    label: string ,
+    rolesLista: Roles[] , 
 }
 
-export default function SelectMultiple({ label, rolesLista, value }: Props) {
-    const options = rolesLista.map((role) => ({
-        label: role.description,
-        value: role.name,
-    }));
-
+export default function SelectMultiple({ label, rolesLista , ...rest }: Props){
     return(
         <div className={styles.divInput}>
             <label className={styles.label}>{label}:</label>
-            <Select
-                className={styles.input}
-                options={options}
-                isMulti={true}
-                value={options.filter((option) => value?.includes(option.value))}
-            />
+            <select className={styles.input} {...rest}>
+            {rolesLista.map(o => (
+                <option key={o.name} value={o.name} >{o.description}</option>
+            ))}
+            </select>
         </div>
-    );
+    )
 }
+
